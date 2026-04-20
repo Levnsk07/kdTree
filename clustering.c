8c4f8b6e217e7c1788ea7262db48e9aa71008519
Levnsk07 <levnsk07@gmain.com>
2026-04-20 16:36:17 +0700

    fix dbScan

--- clustering.c
+++ clustering.c
@@ -2,177 +2,87 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-
 #include "kdTree.h"
 
+
 typedef struct {
-    Tree **points;
-    int count;
-    int capacity;
-} Neighbors;
-
-Neighbors *createNeighbors(int capacity) {
-    Neighbors *neighbors = (Neighbors *)malloc(sizeof(Neighbors));
-    neighbors->points = (Tree **)malloc(capacity * sizeof(Tree *));
-    neighbors->count = 0;
-    neighbors->capacity = capacity;
-    return neighbors;
-}
+    int clusterId; // ID кластера (-1: шум, 0: не присвоен)
+    int isVisited; // Посещена ли точка
+    int isCore; // Является ли точка ядром (>= minPts)
+    int isNoise; // Шумовая точка
+} PointInfo;
 
 
-void freeNeighbors(Neighbors *neighbors) {
-    if (neighbors) {
-        free(neighbors->points);
-        free(neighbors);
-    }
-}
 
+void findNeighborsInRadius(Tree *node, double *point, int dim, double eps, Tree ***neighbors, int *numNeighbors) { // если точка в досигаемости, то добавляет в соседи
+    if (node == NULL) return;
 
-void addNeighbor(Neighbors *neighbors, Tree *point) {
-    if (neighbors->count < neighbors->capacity) {
-        neighbors->points[neighbors->count++] = point;
-    }
-}
-
-
-void findNeighbors(Tree *tree, double *point, int dim, double eps, Neighbors *neighbors) {
-    if (tree == NULL) return;
-
-    double dist = distance(tree->point, point, dim);
+    double dist = distance(node->point, point, dim);
     if (dist <= eps) {
-        addNeighbor(neighbors, tree);
+        (*neighbors)[*numNeighbors] = node;
+        (*numNeighbors)++;
+        Tree **temp = realloc(*neighbors, (*numNeighbors + 1) * sizeof(Tree *));
+        if (temp) *neighbors = temp;
     }
 
-    int axis = tree->splitByDim;
-    if (point[axis] - eps <= tree->point[axis]) {
-        findNeighbors(tree->left, point, dim, eps, neighbors);
-    }
-    if (point[axis] + eps >= tree->point[axis]) {
-        findNeighbors(tree->right, point, dim, eps, neighbors);
+    int axis = node->splitByDim;
+    double splitDist = fabs(point[axis] - node->point[axis]);
+
+    if (point[axis] < node->point[axis]) {
+        findNeighborsInRadius(node->left, point, dim, eps, neighbors, numNeighbors);
+        if (splitDist <= eps) {
+            findNeighborsInRadius(node->right, point, dim, eps, neighbors, numNeighbors);
+        }
+    } else {
+        findNeighborsInRadius(node->right, point, dim, eps, neighbors, numNeighbors);
+        if (splitDist <= eps) {
+            findNeighborsInRadius(node->left, point, dim, eps, neighbors, numNeighbors);
+        }
     }
 }
 
-
-void expandCluster(Tree *tree, Tree *point, int dim, double eps, int minPts, int clusterId,
-                   int *clusterIds, double **points, int numPoints) {
-    Neighbors *neighbors = createNeighbors(numPoints);
-    findNeighbors(tree, point->point, dim, eps, neighbors);
-
-    if (neighbors->count < minPts) {
-        freeNeighbors(neighbors);
-        return;
-    }
-
-    for (int i = 0; i < neighbors->count; i++) {
-        Tree *neighbor = neighbors->points[i];
-        int neighborIndex = -1;
-        for (int j = 0; j < numPoints; j++) {
-            int isEqual = 1;
-            for (int k = 0; k < dim; k++) {
-                if (neighbor->point[k] != points[j][k]) {
-                    isEqual = 0;
-                    break;
-                }
-            }
-            if (isEqual) {
-                neighborIndex = j;
-                break;
-            }
-        }
-        if (neighborIndex != -1 && clusterIds[neighborIndex] == -1) {
-            clusterIds[neighborIndex] = clusterId;
-            expandCluster(tree, neighbor, dim, eps, minPts, clusterId, clusterIds, points, numPoints);
-        }
-    }
-    freeNeighbors(neighbors);
-}
-
-
-int *dbscan(Tree *tree, double **points, int num_points, int dim, double eps, int minPts) {
-    int *clusterIds = (int *)calloc(num_points, sizeof(int));
+int* dbscan(Tree *root, double **points, int numPoints, int dim, double eps, int minPts) {
+    PointInfo *clusterInfo = calloc(numPoints, sizeof(PointInfo));
     int clusterId = 0;
 
-    for (int i = 0; i < num_points; i++) {
-        if (clusterIds[i] != -1) continue;
+    for (int i = 0; i < numPoints; i++) {
+        if (clusterInfo[i].isVisited) continue;
+        clusterInfo[i].isVisited = 1;
 
-        Neighbors *neighbors = createNeighbors(num_points);
-        findNeighbors(tree, points[i], dim, eps, neighbors);
+        Tree **neighbors = malloc(numPoints * sizeof(Tree *));
+        int numNeighbors = 0;
+        findNeighborsInRadius(root, points[i], dim, eps, &neighbors, &numNeighbors);
 
-        if (neighbors->count < minPts) {
-            clusterIds[i] = -1; // Шум
+        if (numNeighbors < minPts) {
+            clusterInfo[i].isNoise = 1;
         } else {
             clusterId++;
-            clusterIds[i] = clusterId;
-            expandCluster(tree, neighbors->points[0], dim, eps, minPts, clusterId, clusterIds, points, num_points);
-        }
-        freeNeighbors(neighbors);
-    }
+            clusterInfo[i].clusterId = clusterId;
+            clusterInfo[i].isCore = 1;
 
-    return clusterIds;
-}
-
-
-FuzzyCMeansResult cmean(double **points, int num_points, int dim, int num_clusters, double m) {
-    FuzzyCMeansResult result;
-    result.centroids = (double **)malloc(num_clusters * sizeof(double *));
-    result.memberships = (double **)malloc(num_points * sizeof(double *));
-    result.num_clusters = num_clusters;
-    result.num_points = num_points;
-
-    for (int i = 0; i < num_clusters; i++) {
-        result.centroids[i] = (double *)malloc(dim * sizeof(double));
-        for (int j = 0; j < dim; j++) {
-            result.centroids[i][j] = (double)rand() / RAND_MAX * 10.0;
-        }
-    }
-
-    for (int i = 0; i < num_points; i++) {
-        result.memberships[i] = (double *)malloc(num_clusters * sizeof(double));
-    }
-
-    for (int iter = 0; iter < 100; iter++) {
-        for (int i = 0; i < num_points; i++) {
-            for (int j = 0; j < num_clusters; j++) {
-                double sum = 0.0;
-                for (int k = 0; k < num_clusters; k++) {
-                    double dist_ij = distance(points[i], result.centroids[j], dim);
-                    double dist_ik = distance(points[i], result.centroids[k], dim);
-                    sum += pow(dist_ij / dist_ik, 2.0 / (m - 1.0));
+            for (int j = 0; j < numNeighbors; j++) {
+                int neighborIndex = -1;
+                for (int k = 0; k < numPoints; k++) {
+                    if (neighbors[j]->point == points[k]) {
+                        neighborIndex = k;
+                        break;
+                    }
                 }
-                result.memberships[i][j] = 1.0 / sum;
-            }
-        }
-
-        for (int j = 0; j < num_clusters; j++) {
-            double sum_weights = 0.0;
-            for (int k = 0; k < dim; k++) {
-                result.centroids[j][k] = 0.0;
-            }
-            for (int i = 0; i < num_points; i++) {
-                double weight = pow(result.memberships[i][j], m);
-                for (int k = 0; k < dim; k++) {
-                    result.centroids[j][k] += weight * points[i][k];
+                if (neighborIndex != -1 && !clusterInfo[neighborIndex].isVisited) {
+                    clusterInfo[neighborIndex].isVisited = 1;
+                    if (numNeighbors >= minPts) {
+                        clusterInfo[neighborIndex].clusterId = clusterId;
+                        clusterInfo[neighborIndex].isCore = 1;
+                    }
                 }
-                sum_weights += weight;
-            }
-            for (int k = 0; k < dim; k++) {
-                result.centroids[j][k] /= sum_weights;
             }
         }
+        free(neighbors);
     }
-
+    int *result = malloc(numPoints * sizeof(int));
+    for (int i = 0; i < numPoints; i++) {
+        result[i] = clusterInfo[i].isNoise ? -1 : clusterInfo[i].clusterId;
+    }
+    free(clusterInfo);
     return result;
-}
-
-
-void freeFuzzyCMeansResult(FuzzyCMeansResult *result) {
-    if (!result) return;
-    for (int i = 0; i < result->num_clusters; i++) {
-        free(result->centroids[i]);
-    }
-    free(result->centroids);
-    for (int i = 0; i < result->num_points; i++) {
-        free(result->memberships[i]);
-    }
-    free(result->memberships);
 }
